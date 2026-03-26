d6514952fee0f35975147c12d471ad33088c255a
Yash Mane <yashmane9872@gmail.com>
2026-03-26 23:41:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "App Running 🚀 test1 test2 test3 test4 test5 test6 test7 test8"
+    return "App Running 🚀 test1 test2 test3 test4 test5 test6 test7 test8 test9(user bash in bg)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
