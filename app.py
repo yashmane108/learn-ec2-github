ef58d13d29ae2ca78f27979ab77d192f96ff0bd9
Yash Mane <yashmane9872@gmail.com>
2026-03-26 21:09:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Project Running Successfully 🚀 update version"
+    return "CI/CD Project Running Successfully 🚀 update version. time 21:09"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
