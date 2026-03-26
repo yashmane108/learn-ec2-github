03e2413d40eecce8f783b20dfef5eec3dc33b524
Yash Mane <yashmane9872@gmail.com>
2026-03-26 22:34:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "App Running 🚀 test1 test2"
-
-@app.route('/deploy')
-def deploy():
-    os.system("bash /home/ec2-user/cicd/deploy.sh")
-    return "Deployment Triggered changes in github 2nd time 3rd time after deploy.sh code change"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
